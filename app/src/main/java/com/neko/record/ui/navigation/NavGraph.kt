443c797bc8968f388260aaa8edb4d33db4d888b2
swtneko <nekyo.tc@gmail.com>
2026-07-07 18:35:23 +0700

    fix: add missing padding import in NavGraph.kt

--- app/src/main/java/com/neko/record/ui/navigation/NavGraph.kt
+++ app/src/main/java/com/neko/record/ui/navigation/NavGraph.kt
@@ -2,6 +2,7 @@ package com.neko.record.ui.navigation
 
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
